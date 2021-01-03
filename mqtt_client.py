88c096fc7833e7e06ccddeae8d4d06cbc3878faf
Allan Elsberry <allan.elsberry@gmail.com>
2021-01-03 16:07:49 -0500

    fixed frozen imu

--- mqtt_client.py
+++ mqtt_client.py
@@ -30,7 +30,10 @@ def on_message(client, userdata, msg):
   # print( f"{data['euler'][0]}", end='\r')
   save_pickle(data,'sensor.pickle')
   new_data=load_pickle('sensor.pickle')
-  print( f"{new_data.get('euler')[0]}", end='\r')
+  try:  
+    print( f"{new_data.get('euler')[0]}", end='\r')
+  except:
+    print('No data available')
 
   #client.disconnect()
     
