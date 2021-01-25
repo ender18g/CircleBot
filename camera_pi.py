4624efdb129a3b1e9198d24fe3b2712a13bc541f
Allan Elsberry <allan.elsberry@gmail.com>
2021-01-25 14:37:27 -0500

    updated framerate / resolution

--- camera_pi.py
+++ camera_pi.py
@@ -7,7 +7,7 @@ from base_camera import BaseCamera
 class Camera(BaseCamera):
     @staticmethod
     def frames():
-        with picamera.PiCamera() as camera:
+        with picamera.PiCamera(resolution=(640,480),framrate=10) as camera:
             # let camera warm up
             time.sleep(2)
 
